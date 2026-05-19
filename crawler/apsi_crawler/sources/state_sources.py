b1f64c8d6abc7eb8f3e65d09f4b9bcd9f587563c
sakya-li <sakya@sakya-li.local>
2026-05-19 19:55:52 +0800

    feat: add Illinois BidBuy HTML adapter

--- crawler/apsi_crawler/sources/state_sources.py
+++ crawler/apsi_crawler/sources/state_sources.py
@@ -1,6 +1,7 @@
 from apsi_crawler.sources.base import Source
 from apsi_crawler.spiders.ca_caleprocure import fetch_ca_caleprocure_opportunities
 from apsi_crawler.spiders.fl_mfmp import fetch_fl_mfmp_opportunities
+from apsi_crawler.spiders.il_bidbuy import fetch_il_bidbuy_opportunities
 from apsi_crawler.spiders.ny_contract_reporter import fetch_ny_contract_reporter_opportunities
 from apsi_crawler.spiders.state_fixture import load_state_fixture_opportunities
 from apsi_crawler.spiders.tx_esbd import fetch_tx_esbd_opportunities
@@ -50,5 +51,6 @@ STATE_SOURCES = {
         jurisdiction="state",
         state_code="IL",
         fixture_loader=load_state_fixture_opportunities,
+        live_fetcher=fetch_il_bidbuy_opportunities,
     ),
 }
